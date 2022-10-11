4370300433624cd7afe9a1f4969b76e29054b106
Dielle Lopes <105114923+ly1359@users.noreply.github.com>
2022-10-11 11:14:52 -0300

    Create vetorQ13.c

---
+++ vetorQ13.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void inverte(int n, int *vet){
+    int rotev[9];
+    for(int j = 9,i = 0; i <= 9; i++,j--){
+        rotev[i] = vet[j];
+        printf("%d ", rotev[i]);
+    }
+  return;
+}
+
+int main(){
+  int vetor[9];
+  printf("Digite o número dos elementos no vetor:\n");
+    
+  for(int i = 0; i < 9; i++){
+    scanf("%d", &vetor[i]);
+  }
+
+  printf("Vetor A:\n");
+  for(int i = 0; i < 9; i++){
+    printf("%d ", vetor[i]);
+  }
+  printf("\nVetor invertido:\n");
+  inverte(9, vetor);
+
+  return 0;
+}
